15cf16a17ae6678990ec747503ec3e62a36b1c25
Carlos Cardoso <cvcardos5@gmail.com>
2025-05-29 22:51:16 -0300

    haskell

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,24 +1,22 @@
 menorCaminho :: [[Int]] -> Int
+-- Caso da matriz vazia
 menorCaminho [] = 0
-menorCaminho matriz = 
-    if length (head matriz) == 1 && length matriz == 1 -- [[1]]
-        then return head (head matriz)
-    if length (head matriz) == 1 -- [[1, 2, 3]]
-        then return (head (head matriz) + menorCaminho (tail matriz))
-    if length matriz == 1 -- [[1], [2], [3]]
-        then return (head (head matriz) + menorCaminho (map tail matriz))
-    else do
-        let caminho_direita = head (head matriz) + menorCaminho (map tail matriz)
-        let caminho_baixo = head (head (tail matriz)) + menorCaminho (tail matriz)
-        return (min caminho_direita caminho_baixo)
+
+-- Caso de matriz 1x1
+menorCaminho [[x]] = x
+
+-- Caso de uma única linha
+menorCaminho [linha] = sum linha
+
+-- Caso de uma única coluna
+menorCaminho matriz | all (\linha -> length linha == 1) matriz = sum (map head matriz)
+
+-- Caso geral (matriz com mais de uma linha e mais de uma coluna)
+menorCaminho ((x:xs):ys) =
+    x + min (menorCaminho (xs : ys)) (menorCaminho (map (x:) ys))
+
 
 main :: IO ()
 main = do
-    let matriz = [
-        [5, 8, 0, 7, 4, 9, 6],
-        [3, 1, 3, 6, 1, 3, 4],
-        [5, 2, 9, 2, 5, 0, 3],
-        [8, 4, 0, 0, 6, 1, 5],
-        [0, 9, 0, 8, 4, 9, 3],
-    ]
-    print $ menorCaminho matriz
+  let matriz = [[5, 8, 0, 7, 4, 9, 6], [3, 1, 3, 6, 1, 3, 4], [5, 2, 9, 2, 5, 0, 3], [8, 4, 0, 0, 6, 1, 5], [0, 9, 0, 8, 4, 9, 3]]
+  print $ menorCaminho matriz
